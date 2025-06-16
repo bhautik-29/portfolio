905e0de090af54795c795bee7ef91fac6cddfc86
bhautik-29 <bhautikk666@gmail.com>
2025-06-16 20:53:48 +0530

    code done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
-window.onload = function() {
+const navbar = () => {
   var navitems = document.querySelector('.nav-items');
+
+
+
 
   function initializeMenu() {
       if (window.innerWidth < 900) {
@@ -29,13 +32,54 @@ window.onload = function() {
       }
   }
 
-  // Initialize the nav-items based on window width
+  
   initializeMenu();
 
-  // Add click event listener to the menu icon
+  
   document.getElementById('menu-icon').addEventListener('click', openMenu);
   document.getElementById('menu-close-button').addEventListener('click', closeMenu);
 
-  // Optionally, update the menu state when the window is resized
+  
   window.addEventListener('resize', initializeMenu);
 };
+
+navbar()
+
+
+let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+// scoll section
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            // active navbar links
+            navlinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+            // active section for Animation
+            sec.classList.add('show-animate');
+
+        }
+        // if want to use animation that repeats on scroll use this
+        else{
+            sec.classList.remove('show-animate')
+        }
+    })
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky' , window.scrollY > 100);
+
+    let footer = document.querySelector('footer');
+
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight)
+}
+
+
